a2c6fbf8445daccf906b9db42535ca63b4efab5b
Tarik Guney <atarikguney@gmail.com>
2019-08-17 11:34:27 -0600

    Added performance tests project and updated the solution

---
+++ GenericPipeline.PerformanceTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericPipeline.PerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
